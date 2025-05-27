98ea62dc5bfb8ce1af5b00d32bc84f4260cbe106
edowaado29 <eidowaado@gmail.com>
2025-05-27 07:38:59 +0700

    update view:login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
             <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
               <div class="card card-plain">
                 <div class="card-header pb-0 text-start">
-                  <h4 class="font-weight-bolder">Halaman Login</h4>
+                  <h4 class="font-weight-bolder">Halaman Login ...</h4>
                   <p class="mb-0">Masukkan email dan password yang benar!</p>
                 </div>
                 <div class="card-body">
